9e52dc91548472da81efa64a15fcadb82304d64d
Anderson <adnrepper@gmail.com>
2020-08-25 18:06:07 -0300

    Conexão com banco de dados, agora no pacote correto

---
+++ src/main/java/com/bwn/todo/database/connection/DBConnection.java
@@ -0,0 +1,39 @@
+package com.bwn.todo.database.connection;
+
+import java.sql.*;
+
+public class DBConnection {
+
+    private static String url = "jdbc:postgresql://localhost:5432/todo_db";
+    private static String user = "postgres";
+    private static String password = "12345";
+    private static Connection connection;
+
+    static {
+        connect();
+    }
+
+    public static void connect() {
+        try {
+            if (connection == null) {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(url, user, password);
+                connection.setAutoCommit(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
